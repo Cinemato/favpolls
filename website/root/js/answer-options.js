2dfa080ad31099deace73bcf92125d02f7fa8609
Cinemato <alharerekhaled@gmail.com>
2023-07-04 21:07:10 +0300

    Started with create poll form and finished "add" and "remove" option buttons

---
+++ website/root/js/answer-options.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+    $(".side-button").click(() => {
+        let option = `
+        <div class="form-option">
+            <input type="text" name="option${$(".form-option").length + 1}" placeholder="Option ${$(".form-option").length + 1}">
+            <img src="root/images/icons/cross.svg" width="30px" class="remove-option" onclick="removeOption(this)">
+        </div>`
+
+        $(".form-options").append(option);
+        $(".remove-option").css("display", "block");
+    })
+})
+
+const removeOption = (target) => {
+    $(document).ready(() => {
+        $(target).parent().remove();
+
+        if($(".form-option").length <= 2) {
+            $(".remove-option").css("display", "none");
+        }
+
+        $(".form-option").each((index, option) => {
+            $(option).find("input").attr("placeholder", `Option ${index + 1}`);
+            $(option).find("input").attr("name", `option${index + 1}`);
+        })
+    })
+}
